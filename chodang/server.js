892a5898ec6264f83658b9af2fd253be9325bea3
chuwg <gi828282@naver.com>
2023-07-18 02:33:15 +0900

    데이터 전송 출력

--- chodang/server.js
+++ chodang/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(8080, function () {
     console.log("listening on 8080")
@@ -7,4 +9,13 @@ app.listen(8080, function () {
 
 app.get('/', function (require, response) {
     response.sendFile(__dirname + '/index.html');
+});
+
+app.get('/write', function (require, response) {
+    response.sendFile(__dirname + '/write.html');
+});
+
+app.post('/add', function (require, response) {
+    response.send("전송 완료");
+    console.log(require.body)
 });
