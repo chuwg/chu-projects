33c690e6cf68d863f725c3dd4b4bacb7b0443f81
chuwg <gi828282@naver.com>
2023-08-07 01:29:09 +0900

    리스트 페이지 만들기

--- chodang/server.js
+++ chodang/server.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const MongoClient = require('mongodb').MongoClient;
+app.set('view engine', 'ejs');
+
 var db;
 // db 변수 필요 -> 간단하게 사용하기 위해서 db변수에 저장해놓을 예정.
 MongoClient.connect('mongodb+srv://chuwg:Wndndrl8956!@chuwg.whutf7y.mongodb.net/?retryWrites=true&w=majority', function (에러, client) {
@@ -12,17 +14,11 @@ MongoClient.connect('mongodb+srv://chuwg:Wndndrl8956!@chuwg.whutf7y.mongodb.net/
 
     db = client.db('Chodang'); // Chodang 이라는 database(폴더) 연결
 
-    db.collection('post').insertOne({ 이름: 'chuwg', 나이: '37' }, function (에러, 결과) {
-        console.log('저장완료');
-    });
-
-
     app.listen(8080, function () {
         console.log("listening on 8080")
     });
 
 });
-
 
 app.get('/', function (require, response) {
     response.sendFile(__dirname + '/index.html');
@@ -38,10 +34,16 @@ app.post('/add', function (require, response) {
     console.log(require.body.selectOption);
     console.log(require.body.writeInputPassword);
     console.log(require.body.writeCheckBox);
-
-
     db.collection('post').insertOne({ 주문자: require.body.inputName, 수량: require.body.selectOption, 패스워드: require.body.writeInputPassword, 주문동의: require.body.writeCheckBox }, function (에러, 결과) {
+    });
+    // post 라는 컬렉션에서 인설트원하면 내가 원하는 코드 하나를 넣을 수 있음.
+});
 
+
+app.get('/list', function (require, response) {
+    db.collection('post').find().toArray(function (error, result) {
+        console.log(result);
+        response.render('list.ejs', { posts: result });
     });
 
 
