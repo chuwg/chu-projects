e9748ef50f97a6d1af5391b62cf857032dc63d79
웅기 <32463237+chuwg@users.noreply.github.com>
2024-01-04 16:03:58 +0900

    Delete gRPC directory

--- gRPC/ch02/gRPC_test_01/src/main/java/com/example/grpc_test_01/ProductInfoImpl.java
+++
@@ -1,6 +0,0 @@
-package com.example.grpc_test_01;
-
-import grpc_test_01.ProductInfoGrpc;
-
-public class ProductInfoImpl extends ProductInfoGrpc.ProductInfoImplBase {
-}
--- gRPC/product-info-service/app/src/main/java/ecommerce/controller/HelloController.java
+++
@@ -1,7 +0,0 @@
-package ecommerce.controller;
-import org.springframework.web.bind.annotation.GetM
-@RestController
-@Lombok
-public class HelloController {
-    @GetMapping
-}
