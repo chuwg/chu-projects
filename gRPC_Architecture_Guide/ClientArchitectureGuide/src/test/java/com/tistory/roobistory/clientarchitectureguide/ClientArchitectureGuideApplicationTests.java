7504a0e7700ad16195f15fd1976d2167a7ffb8c0
chuwg <gi828282@naver.com>
2024-01-14 11:13:06 +0900

    Client Architecture Guide

---
+++ gRPC_Architecture_Guide/ClientArchitectureGuide/src/test/java/com/tistory/roobistory/clientarchitectureguide/ClientArchitectureGuideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tistory.roobistory.clientarchitectureguide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientArchitectureGuideApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
