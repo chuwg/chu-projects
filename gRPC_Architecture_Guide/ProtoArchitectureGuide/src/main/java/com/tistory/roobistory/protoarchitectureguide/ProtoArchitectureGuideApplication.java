f825557073695404f32896cc3cc20ad20f5d963f
chuwg <gi828282@naver.com>
2024-01-14 11:05:35 +0900

    ProtoArchitectureGuide

---
+++ gRPC_Architecture_Guide/ProtoArchitectureGuide/src/main/java/com/tistory/roobistory/protoarchitectureguide/ProtoArchitectureGuideApplication.java
@@ -0,0 +1,13 @@
+package com.tistory.roobistory.protoarchitectureguide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtoArchitectureGuideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProtoArchitectureGuideApplication.class, args);
+    }
+
+}
