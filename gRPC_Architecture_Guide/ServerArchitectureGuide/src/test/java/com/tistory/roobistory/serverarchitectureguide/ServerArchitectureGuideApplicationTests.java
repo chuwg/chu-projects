2f0048589ffe3b40011b140692039c8061141c06
chuwg <gi828282@naver.com>
2024-01-14 11:08:52 +0900

    Server Architecture Guide

---
+++ gRPC_Architecture_Guide/ServerArchitectureGuide/src/test/java/com/tistory/roobistory/serverarchitectureguide/ServerArchitectureGuideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tistory.roobistory.serverarchitectureguide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerArchitectureGuideApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
