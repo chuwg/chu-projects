d4869f5e6946059988a9296c5f088d0d8cc51fde
chuwg <gi828282@naver.com>
2024-01-18 10:00:13 +0900

    얄코님 java 강의 보면서 실습해보기 시작

---
+++ JavaTest/ch03/ex05/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // 디버깅 연습해보기
+        int age = 38;
+        double height = 176.4;
+        boolean isMarried = true;
+        char sex = 'M';
+        String name = "chuwg";
+    }
+}
