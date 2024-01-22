99c8653713774e92182965c978d708efa2425344
chuwg <gi828282@naver.com>
2024-01-22 13:44:29 +0900

    yaml 파일 업로드

---
+++ Udemy/learn-jpa-and-hibernate/src/main/java/com/tistory/roobirona/learnjpaandhibernate/course/jdbc/CourseJdbcCommandLineRunner.java
@@ -0,0 +1,14 @@
+package com.tistory.roobirona.learnjpaandhibernate.course.jdbc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+
+public class CourseJdbcCommandLineRunner implements CommandLineRunner {
+    @Autowired
+    private CourseJdbcRepository repository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        repository.insert();
+    }
+}
---
+++ Udemy/learn-jpa-and-hibernate/src/main/java/com/tistory/roobirona/learnjpaandhibernate/course/jdbc/CourseJdbcRepository.java
@@ -0,0 +1,21 @@
+package com.tistory.roobirona.learnjpaandhibernate.course.jdbc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class CourseJdbcRepository {
+    @Autowired
+    private JdbcTemplate springJdbcTemplate;
+    private static String INSERT_QUERY =
+            """
+                    insert into course (id, name, author)
+                    values (1, 'Learn AWS', 'in28minutes');
+            """;
+    // """ 는 텍스트 블록. 원하는 타입을 넣을 수 있다.
+    // 텍스트 블록의 장점은 쿼리 형식을 그대로 유지할 수 있다.
+    public void insert() {
+        springJdbcTemplate.update(INSERT_QUERY);
+    }
+}
